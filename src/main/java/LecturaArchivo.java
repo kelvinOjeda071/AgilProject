91c4e6d42f14868d495dd9aeb8e2e8b3267c8ded
kelvinOjeda071 <kelvin.ojeda.2020b@gmail.com>
2022-05-28 09:10:25 -0500

    Prueba N:. 4 Tipo de prueba: @BeforeClass

---
+++ src/main/java/LecturaArchivo.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class LecturaArchivo {
+
+
+    public ArrayList cargarCelulares(String direccionDelArchivo) {
+        ArrayList<HashMap> celulares = new ArrayList<HashMap>();
+        try {
+            BufferedReader bf = new BufferedReader(new FileReader(direccionDelArchivo));
+            String bfRead;
+            while ((bfRead = bf.readLine()) != null) {
+                HashMap<String, String> informacionCelular = new HashMap<String, String>();
+                String[] res = bfRead.split("[,]", 0);
+                informacionCelular.put("modelo", res[0]);
+                informacionCelular.put("nombreMarca", res[1]);
+                informacionCelular.put("codigoImei", res[2]);
+                informacionCelular.put("fechaCaducidad", res[3]);
+                informacionCelular.put("descripcion", res[4]);
+                informacionCelular.put("precio", res[5]);
+                informacionCelular.put("precioCompra", res[6]);
+                celulares.add((informacionCelular));
+            }
+            //txt = txtTemp;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return celulares;
+    }
+
+
+
+
+
+}
--- src/test/java/GananciaTest.java
+++ src/test/java/GananciaTest.java
@@ -1,12 +1,26 @@
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import javax.swing.filechooser.FileSystemView;
 
 import static org.junit.Assert.*;
 
 public class GananciaTest {
     Ganancia g = null;
     private static final double DELTA = 1e-15;
+
+
+    @BeforeClass
+    public static void given_AfileDirectory_when_cargarArchivos_then_ok(){
+        LecturaArchivo lecturaDeArchivo = new LecturaArchivo();
+        int expect = 3;
+        int actual = lecturaDeArchivo.cargarCelulares(FileSystemView.
+                getFileSystemView().getDefaultDirectory().getPath()+"\\AgilProject\\celulares.txt").size();
+        assertEquals(actual, expect);
+        System.out.println("Lectura de archivos exitosa");
+    }
     @Before
     public void setUp(){
         g = new Ganancia();
