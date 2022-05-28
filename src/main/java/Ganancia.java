f90d7bfdbbe9874a6f2903e4d2c3d2c58e77759f
kelvinOjeda071 <kelvin.ojeda.2020b@gmail.com>
2022-05-28 07:18:18 -0500

    Prueba N:. 2 Tipos de prueba: @Before

--- src/main/java/Ganancia.java
+++ src/main/java/Ganancia.java
@@ -1,7 +1,15 @@
 public class Ganancia {
+
+    public float porcentajeGanacia(float precioDeVenta, float precioCompraEmpresa) {
+        float ganancia = calcularGanancia(precioDeVenta, precioCompraEmpresa);
+        float porcentajeDeGanancia = Math.round((ganancia / precioCompraEmpresa) * 100 * 100d) / 100;
+        return porcentajeDeGanancia;
+    }
+
     public float calcularGanancia(float precioDeVenta, float precioCompraEmpresa) {
         float ganancia = precioDeVenta - precioCompraEmpresa;
         return ganancia;
     }
+
 
 }
--- src/test/java/GananciaTest.java
+++ src/test/java/GananciaTest.java
@@ -1,9 +1,13 @@
+import org.junit.After;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class GananciaTest {
     private static final double DELTA = 1e-15;
+    private static Ganancia g;
+
+
 
     @Test
     public void given_two_prices_float_when_calcularGanancia_then_ok() {
@@ -13,5 +17,13 @@ public class GananciaTest {
         float actual = g.calcularGanancia(250, 200);
         assertEquals(expected, actual, DELTA);
 
+    }
+    @After
+    public void give_two_prices_float_when_porcentajeGanacia_then_ok(){
+        Ganancia g = new Ganancia();
+        System.out.println("Prueba N:.2");
+        float expected = Math.round(45.45*100d)/100;
+        float actual = g.porcentajeGanacia(160, 110);
+        assertEquals(expected, actual, DELTA);
     }
 }
