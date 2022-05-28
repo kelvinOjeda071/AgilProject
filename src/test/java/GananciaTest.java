0471f88d9eb4a4e3922332f2d261d863bfd3c008
kelvinOjeda071 <kelvin.ojeda.2020b@gmail.com>
2022-05-28 07:26:04 -0500

    Prueba N:. 2 Tipos de prueba: @After

--- src/test/java/GananciaTest.java
+++ src/test/java/GananciaTest.java
@@ -5,17 +5,11 @@ import static org.junit.Assert.*;
 
 public class GananciaTest {
     private static final double DELTA = 1e-15;
-    Ganancia g;
-
-    private void setUp(){
-        g = new Ganancia();
-    }
-
-
 
     @Test
     public void given_two_prices_float_when_calcularGanancia_then_ok() {
         System.out.println("Prueba N:. 1");
+        Ganancia g = new Ganancia();
         float expected = 50;
         float actual = g.calcularGanancia(250, 200);
         assertEquals(expected, actual, DELTA);
@@ -23,6 +17,7 @@ public class GananciaTest {
     }
     @After
     public void give_two_prices_float_when_porcentajeGanacia_then_ok(){
+        Ganancia g = new Ganancia();
         System.out.println("Prueba N:.2");
         float expected = Math.round(45.45*100d)/100;
         float actual = g.porcentajeGanacia(160, 110);
