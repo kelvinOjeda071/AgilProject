ea165a28cd237a9b822b2e81b6963b55921fa698
kelvinOjeda071 <kelvin.ojeda.2020b@gmail.com>
2022-05-29 02:50:57 -0500

    Test con Mockito N:. 1 Cálculo de la ganancia mediante la incorporación de Test con Mockito.

---
+++ src/main/java/ec/edu/epn/tdd/ganancia/IGanancia.java
@@ -0,0 +1,6 @@
+package ec.edu.epn.tdd.ganancia;
+
+public interface IGanancia {
+    public float calcularGanancia(float precioDeVenta, float precioCompraEmpresa);
+
+}
---
+++ src/test/java/ec/edu/epn/tdd/ganancia/IGananciaTest.java
@@ -0,0 +1,18 @@
+package ec.edu.epn.tdd.ganancia;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+
+public class IGananciaTest {
+    private static final double DELTA = 1e-15;
+    @Test
+    public void given_twoPrices_when_calcularGanancia_then_ok() {
+        IGanancia ig = Mockito.mock(IGanancia.class);
+        Mockito.when(ig.calcularGanancia(160,110)).thenReturn(50f);
+        assertEquals(50, ig.calcularGanancia(160,110), DELTA);
+    }
+
+}
